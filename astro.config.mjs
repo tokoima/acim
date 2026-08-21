a74cfa54ea213eb050862fd53d1f8566b43dc596
tokoima <acimer5@gmail.com>
2026-08-21 13:32:39 +0900

    First Commit

---
+++ astro.config.mjs
@@ -0,0 +1,28 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+import react from '@astrojs/react';
+
+// https://astro.build/config
+export default defineConfig({
+    integrations: [starlight({
+        title: '奇跡講座(A Course in Miracles)',
+        social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+        sidebar: [
+            {
+                label: 'Guides',
+                items: [
+                    // Each item here is one entry in the navigation menu.
+                    { label: 'Example Guide', slug: 'guides/example' },
+                ],
+            },
+            {
+                label: 'Reference',
+                items: [{ autogenerate: { directory: 'reference' } }],
+            },
+        ],
+		}), react()],
+        site: 'https://tokoima.github.io',
+        base: '/acim',
+});
